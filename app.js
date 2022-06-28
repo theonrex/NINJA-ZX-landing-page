a2d6f6d708ace6153180a3330d2e6e50f62f2b5d
Theonrex <theonrex1@gmail.com>
2022-06-28 21:11:13 +0100

    a landing page

---
+++ app.js
@@ -0,0 +1,39 @@
+var header = document.getElementById("myDIV");
+var btns = header.getElementsByClassName("nav-link");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
+
+
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
---
+++ style.js
@@ -0,0 +1,49 @@
+// var ninja = document.getElementById("ninja");
+// var mybtn = ninja.getElementsByClassName("dot");
+// for (var i = 0; i < mybtn.length; i++) {
+//   mybtn[i].addEventListener("click", function() {
+//   var current = document.getElementsByClassName("changebg");
+//   current[0].className = current[0].className.replace(" changebg", "");
+//   this.className += " changebg";
+//   });
+// }
+
+
+// const changeColor = document.getElementById('dox'),
+//       rcolors      = ['red'];
+// let   colorIndex  = 0;
+
+// changeColor.addEventListener('click', () => {
+//   document.body.style.backgroundColor = rcolors[colorIndex];      
+//   colorIndex = (colorIndex + 1) % rcolors.length
+// });
+
+// const orangeColor = document.getElementById('do'),
+//       oColor      = ['orange'];
+// let   oIndex  = 0;
+
+// orangeColor.addEventListener('click', () => {
+//   document.body.style.backgroundColor = oColor[oIndex];      
+//   oIndex = (oIndex + 1) % bColors.length
+// });
+
+// const blueColor = document.getElementById('dob'),
+//       bColors      = ['blue'];
+// let   bIndex  = 0;
+
+// blueColor.addEventListener('click', () => {
+//   document.body.style.backgroundColor = bColors[bIndex];      
+// });
+
+
+function greenColor() {
+  document.body.style.backgroundColor = "green";
+}
+
+function orangeColor() {
+  document.body.style.backgroundColor = "orangered";
+}
+
+function blueColor() {
+  document.body.style.backgroundColor = "blue";
+}
